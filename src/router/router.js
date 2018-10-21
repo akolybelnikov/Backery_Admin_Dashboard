497cfd2be54107b93cbac0cdeb296b276b3dc370
akolybelnikov <a.kolybelnikov@gmail.com>
2018-10-21 13:10:53 +0200

    verifying user token validity

--- src/router/router.js
+++ src/router/router.js
@@ -31,6 +31,13 @@ function getUser() {
     if (persistedStore) {
         user = persistedStore.user
         if (user && user.signInUserSession) {
+            if (
+                user.signInUserSession.accessToken.payload.exp <
+                Math.floor(Date.now() / 1000)
+            ) {
+                store.commit('setUser', null)
+                return null
+            }
             return user
         }
     }
@@ -114,9 +121,9 @@ const router = new Router({
     ]
 })
 
-router.beforeResolve(async (to, from, next) => {
+router.beforeResolve((to, from, next) => {
     if (to.matched.some(record => record.meta.requiresAuth)) {
-        user = await getUser()
+        user = getUser()
         if (!user) {
             return next({
                 path: 'auth',
